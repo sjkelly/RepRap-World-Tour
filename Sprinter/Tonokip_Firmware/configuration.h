c6341fb715755d7b93f89185df409273773a16b2
Chris Thompson <eagleapex@gmail.com>
2011-06-19 13:06:48 -0400

    adjusting accel min

--- Sprinter/Tonokip_Firmware/configuration.h
+++ Sprinter/Tonokip_Firmware/configuration.h
@@ -32,7 +32,7 @@ long max_travel_acceleration_units_per_sq_second = 150; // Max acceleration in m
 float full_velocity_units = 400; // the units between minimum and G1 move feedrate
 float travel_move_full_velocity_units = 400; // used for travel moves
 float min_units_per_second = 35.0; // the minimum feedrate
-float min_constant_speed_units = 2; // the minimum units of an accelerated move that must be done at constant speed
+float min_constant_speed_units = .1; // the minimum units of an accelerated move that must be done at constant speed
                                     // Note that if the move is shorter than this value, acceleration won't be perfomed,
                                     // but will be done at the minimum between min_units_per_seconds and move feedrate speeds.
 #endif
